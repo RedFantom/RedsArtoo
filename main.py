f1176da1a0711c6c8da4ffd7cbeb13b56d52d069
RedFantom <redfantom@outlook.com>
2015-10-19 12:56:54 +0200

    Made on RPi, problems with writing an interface library

---
+++ main.py
@@ -0,0 +1,12 @@
+# Necessary imports for the program. This version is made on the RPi
+import time
+import random
+import RPi.GPIO as GPIO
+
+# Class for the sensors
+class sensors:
+	def light(position):
+		if(position == 1):
+			
+
+
