351a95bce4c46aa7a2e691ba68031eca9236ebbd
RedFantom <RedFantom@users.noreply.github.com>
2015-10-30 13:51:38 +0100

    Made changes

--- sensors.py
+++ sensors.py
@@ -2,32 +2,36 @@ import time
 import RPi.GPIO as GPIO
 
 # The class for the lightsensors
-class lightsensors(pin):
+class lightsensors:
 	
 	# The constructor to set up the necesssary GPIO pin
-	def __init__(self):
+	def __init__(self, pin):
 		GPIO.setup(pin, GPIO.IN)
 	
+	gpiopin = pin
 	# The function to call when  you want to know whether it's light or dark
 	def light(self):
-		return GPIO.input(pin)
+		return GPIO.input(gpiopin)
 
 # The class for the HC-SR04 distance sensors		
-class distancesensor(pintrig, pinecho):
+class distancesensor:
 	
 	# Set up the GPIO pins
-	def __init__(self):
+	def __init__(self, pintrig, pinecho):
 		GPIO.setup(pintrig, GPIO.OUT)
 		GPIO.setup(pinecho, GPIO.IN)
 	
+	pint = pintrig
+	pine = pinecho
+	
 	# The function that returns the approximate distance to an object
 	def distance(self):
-		GPIO.output(pintrig, True)
+		GPIO.output(pint, True)
         time.sleep(0.00001)
-        GPIO.output(pintrig, False)
-        while(GPIO.input(pinecho) == 0):
+        GPIO.output(pint, False)
+        while(GPIO.input(pine) == 0):
             t_one = time.time()
-        while(GPIO.input(pinecho) == 1):
+        while(GPIO.input(pine) == 1):
             t_two = time.time()
         deltat = t_two - t_one
         return round(deltat * 17150, 0)
