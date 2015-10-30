cc0de5d37ddac6e623b56464c80588486c01473b
RedFantom <RedFantom@users.noreply.github.com>
2015-10-30 18:52:30 +0100

    Fixed a problem with the variables

--- motors.py
+++ motors.py
@@ -4,27 +4,27 @@ import RPi.GPIO as GPIO
 class motors: # pinpos and pinneg are pins on which the terminals from the motor driver are connected on the Raspberry Pi
 	
 	# Shared Values
-	pinp = 0
-	pinn = 0
+	pinpos = 0
+	pinneg = 0
 	
 	# Constructor to set up the motor
 	def __init__(self, pinpos, pinneg):	
 		GPIO.setup(pinpos, GPIO.OUT)
 		GPIO.setup(pinneg, GPIO.OUT)
-		pinp = pinpos
-		pinn = pinneg
+		self.pinpos = pinpos
+		self.pinneg = pinneg
 	
 	# The function that starts and stops the motor
 	def drive(self, direction):
 		if(direction == 1):
-			GPIO.output(pinp, True)
-			GPIO.output(pinn, False)
+			GPIO.output(self.pinpos, True)
+			GPIO.output(self.pinneg, False)
 		else if(direction == -1):
-			GPIO.output(pinn, True)
-			GPIO.output(pinp, False)
+			GPIO.output(self.pinneg, True)
+			GPIO.output(self.pinpos, False)
 		else if(direction == 0):
-			GPIO.output(pinp, False)
-			GPIO.output(pinn, False)
+			GPIO.output(self.pinpos, False)
+			GPIO.output(self.pinneg, False)
 		else:
 			return -1
 	
