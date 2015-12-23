2b017a34465fb6a6be3f6982feae45268a9c3459
RedFantom <redfantom@outlook.com>
2015-12-23 16:06:14 +0100

    minor change

--- sensors.py
+++ sensors.py
@@ -33,14 +33,14 @@ class distancesensor:
 	# The function that returns the approximate distance to an object
 	def distance(self):
 		GPIO.output(self.pintrig, True)
-        time.sleep(0.00001)
-        GPIO.output(self.pintrig, False)
-        while(GPIO.input(self.pinecho) == 0):
+	        time.sleep(0.00001)
+        	GPIO.output(self.pintrig, False)
+	        while(GPIO.input(self.pinecho) == 0):
         		t_one = time.time()
-        while(GPIO.input(self.pinecho) == 1):
+	        while(GPIO.input(self.pinecho) == 1):
         		t_two = time.time()
-        deltat = t_two - t_one
-        return round(deltat * 17150, 0)
+		deltat = t_two - t_one
+      		return round(deltat * 17150, 0)
 
 class soundsensor:
 
